49fd95f27e00df742ee0147f16de82911930ac1a
Anshikay17 <135619357+Anshikay17@users.noreply.github.com>
2024-07-04 18:02:06 +0530

    Update MDP.py

--- MDP.py
+++ MDP.py
@@ -11,9 +11,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Loading the saved models
-diabetes_model = pickle.load(open('C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open('C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/parkinsons_model.sav','rb'))
+diabetes_model = pickle.load(open('rC:/Users/Dell/Desktop/Multiple disease prediction/saved_models/diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open('rC:/Users/Dell/Desktop/Multiple disease prediction/saved_models/heart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('rC:/Users/Dell/Desktop/Multiple disease prediction/saved_models/parkinsons_model.sav','rb'))
 
 # Set page configuration
 st.set_page_config(page_title="MediPrognosis",
