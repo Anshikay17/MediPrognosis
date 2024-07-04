653d638b91a97cd6f1fcd2c8372d3aa0906535ab
Anshikay17 <135619357+Anshikay17@users.noreply.github.com>
2024-07-04 18:38:14 +0530

    Update MDP.py

--- MDP.py
+++ MDP.py
@@ -10,11 +10,19 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# Loading the saved models
-diabetes_model = pickle.load(open(r'C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open(r'C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open(r'C:/Users/Dell/Desktop/Multiple disease prediction/saved_models/parkinsons_model'.sav,'rb'))
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Loading the saved models using dynamic path
+try:
+    diabetes_model = pickle.load(open(os.path.join(working_dir, 'saved_models', 'diabetes_model.sav'), 'rb'))
+    heart_disease_model = pickle.load(open(os.path.join(working_dir, 'saved_models', 'heart_disease_model.sav'), 'rb'))
+    parkinsons_model = pickle.load(open(os.path.join(working_dir, 'saved_models', 'parkinsons_model.sav'), 'rb'))
+except FileNotFoundError as e:
+    print(f"Error loading model: {e}")
+    # Handle the error appropriately, e.g., provide a default model or exit the script.
+except Exception as e:
+    print(f"Error: {e}")
+  
 # Set page configuration
 st.set_page_config(page_title="MediPrognosis",
                    layout="wide",
